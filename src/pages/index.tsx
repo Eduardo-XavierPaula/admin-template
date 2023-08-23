d901a7c28114a7c21d5a22d278f56e20439bd07d
Eduardo Xavier <49372451+Eduardo-XavierPaula@users.noreply.github.com>
2023-08-23 00:05:29 -0300

    Projeto Admin Template
    Estruturando layout e alternação entre temas claro e escuro

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/template/Layout";
+
+export default function Home() {
+  return (
+    <Layout titulo="Página Inicial" subtitulo="Estamos construíndo um template Admin!">
+      <h3>Conteudo!!!</h3>
+    </Layout>
+  )
+}
